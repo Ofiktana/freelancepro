98846686562c24f715b2462d1ae3c386ce32bfa2
Tana Ofik <ofik.tana@gmail.com>
2025-04-06 00:17:40 +0100

    successfully installed chakraUI & tailwindCSS, commenced layout development

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,22 @@
+import { Box, Flex, VStack } from "@chakra-ui/react"
 import { Outlet } from "react-router-dom"
 
 function Layout() {
   return (
-    <>
-      <div>Layout</div>
-      <Outlet />
-    </>
+    <Flex>
+      <Box as='nav' className="w-(--sidebar-width) min-h-(--sidebar-height)" bg='gray.600'>
+        {/*Navigation Sidebar */}
+      </Box>
+      <Box className="w-(--main-width) min-h-(--sidebar-height)" >
+        <Box as='header' bg='white' className="w-(--main-width) h-(--header-height)">
+          {/*Header Section */}
+        </Box>
+        <Box as='main' bg='purple.50' className="w-(--main-width) min-h-(--min-page-height)">
+          {/*Main Content */}
+          <Outlet />
+        </Box>
+      </Box>
+    </Flex>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import { Button, HStack } from "@chakra-ui/react"
 
 function Home() {
+
   return (
-    <div className="bg-yellow-200 w-60 h-60 rounded-2xl border-2 border-red-500 flex justify-center items-center">
+    <div>
       <p className="border font-bold">Hello Home</p>
     </div>
   )
